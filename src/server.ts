390189ebc33f5ea434f1c22ddf89cb09971977a2
Immanuel Currah <immanueli.currah@gmail.com>
2025-02-26 21:37:04 -0500

    try

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import { dirname, join, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import bootstrap from './main.server';
+import * as angularRuntime from '@netlify/angular-runtime'
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
